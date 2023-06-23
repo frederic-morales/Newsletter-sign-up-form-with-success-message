99ad22a9ad7e15ff8ef4cc43631c2ed873bf1c12
tokolaptop23 <tocomorales30@gmail.com>
2023-06-23 14:09:10 -0600

    Ending the proyect

--- code.js
+++ code.js
@@ -1,29 +1,42 @@
 const login = document.getElementById('login-container');
 const success = document.getElementById('success-container');
 const loginButton = document.getElementById('login-button');
-const successButton = document.getElementById('success-button');
 const email = document.getElementById('email');
 const errorMessage = document.getElementById('error-message');
+const imageContainer = document.getElementById('image-container');
 
 //loginButton.click = showSuccess;
 loginButton.addEventListener("click", showSuccess);
+email.addEventListener("mouseout", changeColor);
+
 function showSuccess(event){
-    emailValue = email.value;
-    if(emailValue){
-        console.log("emailValue");
+    if(!(email.value.indexOf("@") === -1)){
         login.style.display = 'none';
         success.style.display = 'block';
     } else {
         errorMessage.style.display = 'block';
-        email.classList.add('change-text','change-input','change-hover');
+        email.classList.add('change-placeholder', 'change-input');
+        email.style.color = "hsl(4, 100%, 67%)";
+        email.style.border = "solid 1px hsl(4, 100%, 67%)";
     };
     event.preventDefault();
 }
+function changeColor(){
+    if(errorMessage.style.display = 'block'){
+        errorMessage.style.display = 'none';    
+        email.classList.remove('change-placeholder', 'change-input');
+        email.style.color = "black";
+        email.style.border = "solid 1px black"
+    }
+}
 
-//successButton.click = showLogin;
+/*
+const successButton = document.getElementById('success-button');
 successButton.addEventListener("click", showLogin);
+successButton.click = showLogin;
 function showLogin(event){
     login.style.display = 'block';
     success.style.display = 'none';
     event.preventDefault();
 }
+*/
